4e21dba7177a90938557394ca3fcb6e2dcc796d6
kenkz447 <kenkz447@gmail.com>
2024-11-08 23:08:34 +0700

    update

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/libs/shadcn-ui/components/button";
 import { EntryTree } from "../entry-tree/entry-tree";
 import { useCurrentUser, useGoogleSignIn } from "@/libs/auth";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/libs/shadcn-ui/components/dropdown-menu";
-import { ChevronRight, FilePlus, FolderPlus, PlusCircle } from "lucide-react";
+import { ChevronRight, Circle, FilePlus, FileText, FolderPlus, PlusCircle, Space } from "lucide-react";
 import { usePopupDialog } from "@/libs/popup";
 import { Entry, generateRxId, useEntries } from "@/libs/rxdb";
 import { CreateEntryForm } from "../forms/create-entry-form";
@@ -35,40 +35,47 @@ export function Sidebar() {
 
 
     return (
-        <div className='h-full py-4 flex flex-col'>
-            <div className="h-12 px-4">
-                <h1>Diario</h1>
+        <div className='h-full flex flex-col'>
+            <div className="h-12 px-4 flex items-center justify-center mb-4">
+                <span className="tracking-wide">D</span>
+                <span className="tracking-wide relative text-primary">
+                    <Circle size={26} strokeWidth={1} />
+                    <FileText size={16} strokeWidth={1.8} className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"/>
+                </span>
+                <span className="tracking-wide">C SPACE</span>
             </div>
-            <div className="grow">
-                <div className="px-[16px] text-xs font-medium text-sidebar-foreground/70 mb-4">
-                    <Input placeholder="Search" />
+            <div className="grow flex flex-col">
+                <div className="grow">
+                    <div className="px-[16px] text-xs font-medium text-sidebar-foreground/70 mb-4">
+                        <Input placeholder="Search" />
+                    </div>
+                    <EntryTree />
                 </div>
-                <EntryTree />
-            </div>
-            <div className="p-4 flex flex-col gap-2">
-                <Separator />
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="w-full flex items-center hover:bg-sidebar-accent">
-                            <PlusCircle />
-                            <span className="inline grow text-left line-height-1">
-                                New
-                            </span>
-                            <ChevronRight />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end" side="right" sideOffset={24} className="w-[200px]">
-                        <DropdownMenuItem onClick={() => onNewEntry('folder')}><FolderPlus /> Folder</DropdownMenuItem>
-                        <DropdownMenuItem onClick={() => onNewEntry('document')}><FilePlus /> Document</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-                {
-                    !currentUser && (
-                        <Button onClick={googleSignIn} className="w-full">
-                            Login
-                        </Button>
-                    )
-                }
+                <div className="p-4 flex flex-col gap-2">
+                    <Separator />
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className="w-full flex items-center hover:bg-sidebar-accent">
+                                <PlusCircle />
+                                <span className="inline grow text-left line-height-1">
+                                    New
+                                </span>
+                                <ChevronRight />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end" side="right" sideOffset={24} className="w-[200px]">
+                            <DropdownMenuItem onClick={() => onNewEntry('folder')}><FolderPlus /> Folder</DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => onNewEntry('document')}><FilePlus /> Document</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    {
+                        !currentUser && (
+                            <Button onClick={googleSignIn} className="w-full">
+                                Login
+                            </Button>
+                        )
+                    }
+                </div>
             </div>
         </div>
     )
